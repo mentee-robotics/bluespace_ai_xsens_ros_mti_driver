3a95154b9c786e5852375ab73936fbe812b69d79
mosheap <moshe@menteebot.com>
2023-05-22 08:59:42 +0000

    intial commit

--- launch/xsens_mti_node.launch.py
+++ launch/xsens_mti_node.launch.py
@@ -1,9 +1,12 @@
 from launch import LaunchDescription
 from launch.actions import SetEnvironmentVariable
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 from pathlib import Path
 from os import getenv, path
+import os
 
 def generate_launch_description():
 
@@ -12,19 +15,21 @@ def generate_launch_description():
     # Set env var to print messages to stdout immediately
     arg = SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
     ld.add_action(arg)
-
-    pkg_shared_dir = get_package_share_directory('bluespace_ai_xsens_mti_driver')
     
-    imu_params_path = path.join(pkg_shared_dir, "params", "xsens_mti_node.yaml")
-    parameters_file_path = Path(imu_params_path)
+    pkg_shared_dir = get_package_share_directory('bluespace_ai_xsens_mti_driver')
+    imu_params = DeclareLaunchArgument('imu_params', 
+                                       default_value=os.path.join(pkg_shared_dir, 'config', 'xsens_mti_node.yaml')
+    )
+    
     xsens_mti_node = Node(
             package='bluespace_ai_xsens_mti_driver',
             executable='xsens_mti_node',
             name='xsens_mti_node',
             output='screen',
-            parameters=[parameters_file_path],
+            parameters=[LaunchConfiguration('imu_params')],
             arguments=[]
             )
+    ld.add_action(imu_params)
     ld.add_action(xsens_mti_node)
 
     return ld
